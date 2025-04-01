24628c6aa5e292ef158b78fb68663fe1ccca968a
leon655 <leongatuiku@yahoo.com>
2025-04-01 15:11:52 +0300

    made changes to getrequest.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,23 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
+        # Initialize with the URL
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Sends GET request and returns the body of the response
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  # Raises an error for bad status codes (4xx, 5xx)
+            return response.text  # Returns the raw body of the response
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
 
     def load_json(self):
-        pass
+        # Calls get_response_body and converts the response to JSON
+        response_body = self.get_response_body()
+        if response_body:
+            return requests.get(self.url).json()  # Convert the JSON data to a Python dictionary/list
+        return None
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,16 +1,17 @@
 from GetRequester import GetRequester
-class GetRequesterTest:
+
+class TestGetRequester:
     '''Class {Classname} in {modulename}.py'''
-URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
+    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+    JSON_STRING = "[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
+    CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
 
-def test_get_response():
+    def test_get_response(self):
         '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+        requester = GetRequester(self.URL)
+        assert(requester.get_response_body() == self.JSON_STRING)
 
-def test_load_json():
+    def test_load_json(self):
         '''load_json function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.load_json() == CONVERTED_DATA)
+        requester = GetRequester(self.URL)
+        assert(requester.load_json() == self.CONVERTED_DATA)
